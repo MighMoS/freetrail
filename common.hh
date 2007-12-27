df149df66be2d6aed6b2add1a55ca6853e70235e
Adam Petaccia <adam@staticvoid.(none)>
2007-12-26 23:59:07 -0500

    Nothing, really

--- common.hh
+++ common.hh
@@ -3,6 +3,7 @@
 
 enum sex_ {MALE, FEMALE};
 enum health_ {DEAD, DYING, POOR, FAIR, GOOD, EXCELLENT};
+enum weather {CLOUDY, PARTLY_CLOUDY, SUNNY, RAINY};
 
 const string select_one = "Select an option: ";
 
--- main.cc
+++ main.cc
@@ -19,7 +19,6 @@ int main (int argc, char **argv)
 
 void main_loop ()
 {
-	//cout << "Please pick an option:\n";
 }
 
 party* init_recruit ()
