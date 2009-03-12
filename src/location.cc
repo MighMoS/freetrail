6b244fe51fdd96c6d7edc656d0d1a7da6aa191c9
Adam Petaccia <adam@tpetaccia.com>
2009-03-11 20:01:26 -0400

    Path::run(): don't tell lies

--- src/location.cc
+++ src/location.cc
@@ -68,25 +68,24 @@ Freetrail::Runner::Status FixedJump::run (Party* party) const
 Freetrail::Runner::Status Path::run(Party* party) const
 {
     Freetrail::Runner::Status stat;
-    const unsigned int speed = party->get_speed();
+    unsigned int speed = party->get_speed();
     unsigned int distance_traveled = 0;
     unsigned int food_eaten;
     bool reached_landmark = false;
 
     while (reached_landmark == false)
     {
-        if (get_next_distance() > speed + distance_traveled)
+        if (get_next_distance() <= speed + distance_traveled)
         {
-            distance_traveled += speed;
-        }
-        else
-        {
-            distance_traveled = get_next_distance() - distance_traveled;
+            speed = get_next_distance() - distance_traveled;
             reached_landmark = true;
         }
+
+        distance_traveled += speed;
         food_eaten = party->eat_food();
 
-        std::cout << "You traveled " << distance_traveled << " miles today and have "
+        std::cout << "You traveled " << speed << " miles today ("
+            << distance_traveled << " miles total) and have "
             << party->get_food() << " lbs of food remaining.\n";
         user_interface::wait_for_key();
     }
