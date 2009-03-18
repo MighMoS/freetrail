a7cf15333142fd6de1c66dd5c90d3b085dc32c85
Adam Petaccia <adam@tpetaccia.com>
2009-03-17 20:28:23 -0400

    Move most parsing logic into its own file.

--- src/parser.hh
+++ src/parser.hh
@@ -1,5 +1,13 @@
+#ifndef PARSERH
+#define PARSERH
 #include <stdexcept>
 #include <string>
+
+#include <glibmm.h>
+#include <libxml++/libxml++.h>
+
+#include "location.hh"
+#include "world.hh"
 
 class MapParsingException : public std::runtime_error
 {
@@ -8,3 +16,19 @@ class MapParsingException : public std::runtime_error
             std::runtime_error(message)
     {};
 };
+
+typedef const xmlpp::Node::NodeList::const_iterator xmlmapIter;
+
+Outpost* fill_outpost (xmlmapIter& iter);
+
+Path* fill_path (xmlmapIter& stop_iter, const Glib::ustring& type);
+
+ForkOptionPtr fill_jump (xmlmapIter& iter);
+
+Fork* fill_fork (xmlmapIter& iter, const Glib::ustring& type);
+
+LocationPtr fill_location (xmlmapIter& iter);
+
+Track* fill_track (xmlmapIter& track_iter);
+
+#endif
