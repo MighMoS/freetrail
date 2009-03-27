c7ee2834ed3d86adeffa3961ef550a046500523b
Adam Petaccia <adam@tpetaccia.com>
2009-03-27 01:03:59 -0400

    Add a simple testsuite

---
+++ test/test_party.cc
@@ -0,0 +1,87 @@
+#include <memory>
+
+#include <cpptest.h>
+
+#include "party.hh"
+
+class TestParty : public Test::Suite
+{
+    void TestConstructor ();
+    void TestAddMember ();
+    void TestActivity ();
+    public:
+    TestParty()
+    {
+        TEST_ADD(TestParty::TestConstructor);
+        TEST_ADD(TestParty::TestAddMember);
+        TEST_ADD(TestParty::TestActivity);
+    };
+};
+
+void TestParty::TestConstructor ()
+{
+    Party some_party;
+
+    TEST_ASSERT (some_party.size () == 0);
+}
+
+void TestParty::TestAddMember ()
+{
+    Party some_party;
+
+    some_party.add_member (Member("John", MALE));
+    some_party.add_member (Member("Mary", FEMALE));
+    some_party.add_member (Member("Paul", MALE));
+
+    TEST_ASSERT (some_party.size () == 3);
+
+    some_party.add_member (Member("Mary", FEMALE));
+    some_party.add_member (Member("Paul", MALE));
+
+    TEST_ASSERT (some_party.size () == 3);
+}
+
+void TestParty::TestActivity ()
+{
+    Party some_party;
+
+    some_party.add_member (Member("John", MALE));
+    some_party.add_member (Member("Mary", FEMALE));
+    some_party.add_member (Member("Paul", MALE));
+
+    TEST_ASSERT (some_party.size () == 3);
+
+    MemberContainer* actives = some_party.get_active_members ();
+    TEST_ASSERT (actives->size () == 3);
+    delete actives;
+
+    actives = some_party.get_inactive_members ();
+    TEST_ASSERT (actives->size () == 0);
+    delete actives;
+
+    Member kenny ("Kenny", MALE);
+    for (unsigned int i = 0; i < 7; i++) kenny.starve ();
+    some_party.add_member (kenny);
+
+    TEST_ASSERT (some_party.size () == 3);
+    actives = some_party.get_active_members ();
+    TEST_ASSERT (actives->size () == 3);
+    delete actives;
+    actives = some_party.get_inactive_members ();
+    TEST_ASSERT (actives->size () == 1);
+    delete actives;
+}
+
+int main ()
+{
+    Test::Suite ts;
+    ts.add(std::auto_ptr<Test::Suite>(new TestParty));
+
+    Test::TextOutput output (Test::TextOutput::Verbose);
+    bool it_worked = ts.run (output);
+
+    if (it_worked)
+        return 0;
+    return 1;
+}
+
